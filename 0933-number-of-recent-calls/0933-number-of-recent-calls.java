a231bcbe40111a5c1f5a2f11d9aa42ee233f53e2
Ansh Gupta <147173599+anshguptasince2004@users.noreply.github.com>
2026-07-20 20:31:57 +0530

    Time: 21 ms (80.99%), Space: 59.7 MB (37.16%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,10 +1,8 @@
 class RecentCounter {
     Queue<Integer> queue;
-    
     public RecentCounter() {
         queue = new LinkedList<>();
     }
-    
     public int ping(int t) {
         queue.offer(t);
         while(queue.peek() < t-3000) {
